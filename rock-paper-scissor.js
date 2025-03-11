1fb4ef82e1bd6a0ff17e5b5a9063d8989e7df4ae
abishek1704-dot <abisheknarayanan.k@prodapt.com>
2025-03-11 16:45:03 +0530

    added

---
+++ rock-paper-scissor.js
@@ -0,0 +1,73 @@
+
+
+const score=JSON.parse(localStorage.getItem('score')) || {win:0,lose:0,tie:0}
+updatestatus()
+function usermove(move){
+
+    const computerMove=pickComputermove();
+    let result='';
+    if(move==='rock'){
+        if (computerMove==='rock'){
+            result='tie';
+        }else if(computerMove==='paper'){
+            result='you lose';
+        }else if(computerMove=='scissores'){
+            result='you win';
+        }
+
+    }else if(move==='paper'){
+        if (computerMove==='rock'){
+        result='you win';
+    }else if(computerMove==='paper'){
+        result='tie';
+    }else if(computerMove==='scissores'){
+        result='you lose';
+    }
+
+    }else if(move==='scissores'){
+        if (computerMove==='rock'){
+        result='you lose';
+    }else if(computerMove==='paper'){
+        result='you win';
+    }else if(computerMove=='scissores'){
+        result='tie';
+    }
+    }
+
+    if(result==='you win'){
+        score.win+=1;
+    }else if(result==='you lose'){
+        score.lose+=1
+    }else if(result==='tie'){
+        score.tie+=1
+    }
+
+    localStorage.setItem('score',JSON.stringify(score));
+    
+    updatestatus()
+
+    document.querySelector('.moves').innerHTML=` you 
+<img src="img-src/${move}-emoji.png" class="move-icon">
+<img src="img-src/${computerMove}-emoji.png" class="move-icon">
+computer`
+
+document.querySelector('.result').innerText=result
+
+}
+
+function updatestatus(){
+    document.querySelector('.status').innerText=`win : ${score.win}, lose : ${score.lose}, tie : ${score.tie}`
+}
+
+function pickComputermove(){
+    const randomNumber=Math.random();
+    let computerMove='';
+    if (randomNumber>=0 && randomNumber<1/3){
+        computerMove='rock';
+    }else if (randomNumber>=1/3 && randomNumber<1/2){
+        computerMove='paper';
+    }else if(randomNumber>=1/2 && randomNumber<1){
+        computerMove='scissores';
+    }
+    return computerMove;
+}
